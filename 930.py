929e8f2f0f348f8f12afaf250f603617330ce63d
Swap <60968556+Bucky09@users.noreply.github.com>
2024-03-15 00:10:03 +0530

    Create 930.py

---
+++ 930.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        i = count = res = 0
+        for j in range(len(nums)):
+            if nums[j] == 1:
+                goal -= 1
+                count = 0
+                      
+            while goal == 0 and i <= j:
+                goal += nums[i]
+                i += 1
+                count += 1
+                if i > j - goal + 1:
+                    break
+                    
+            while goal < 0:
+                goal += nums[i]
+                i += 1
+
+            res += count
+        return res
