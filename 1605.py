3d97658a497fc3b6451fb064d074dee6dff4a103
Swap <60968556+Bucky09@users.noreply.github.com>
2024-07-20 23:17:07 +0530

    Create 1605.py

---
+++ 1605.py
@@ -0,0 +1,20 @@
+class Solution:
+    def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
+        N = len(rowSum)
+        M = len(colSum)
+
+        orig_matrix = [[0] * M for _ in range(N)]
+        i, j = 0, 0
+
+        while i < N and j < M:
+            orig_matrix[i][j] = min(rowSum[i], colSum[j])
+
+            rowSum[i] -= orig_matrix[i][j]
+            colSum[j] -= orig_matrix[i][j]
+
+            if rowSum[i] == 0:
+                i += 1
+            else:
+                j += 1
+        
+        return orig_matrix
