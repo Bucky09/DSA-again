46fa730ac133a84d88815aa85ff9c8347b1562d7
Swap <60968556+Bucky09@users.noreply.github.com>
2024-11-08 23:57:48 +0530

    Create 1829.java

---
+++ 1829.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] getMaximumXor(int[] n, int maximumBit) {
+        int[] res = new int[n.length];
+        int val = (1 << maximumBit) - 1;
+        for (int i = 0; i < n.length; ++i)
+            res[n.length - i - 1] = val ^= n[i];        
+        return res;
+}
+}
