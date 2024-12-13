082064735443daa296502e6e9e65c953491763b3
Swap <60968556+Bucky09@users.noreply.github.com>
2024-12-13 22:37:00 +0530

    Create 2593.py

---
+++ 2593.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findScore(self, nums: List[int]) -> int:
+        n = len(nums)
+        ord = list(range(n))
+        
+        ord.sort(key=lambda x: (nums[x], x))  
+        
+        m = [False] * n  
+        score = 0
+        
+        for x in ord:
+            if m[x]: continue 
+            score += nums[x]
+            m[x] = True  
+            if x > 0: m[x - 1] = True  
+            if x + 1 < n: m[x + 1] = True  
+        
+        return score
