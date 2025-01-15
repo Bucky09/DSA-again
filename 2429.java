cca26908475e69f674f96ccbfcca941d0a0deb1f
Swap <60968556+Bucky09@users.noreply.github.com>
2025-01-16 00:25:31 +0530

    Create 2429.java

---
+++ 2429.java
@@ -0,0 +1,14 @@
+    public int minimizeXor(int num1, int num2) {
+        int a = Integer.bitCount(num1), b = Integer.bitCount(num2), res = num1;
+        for (int i = 0; i < 32; ++i) {
+            if (a > b && ((1 << i) & num1) > 0) {
+                res ^= 1 << i;
+                a--;
+            }
+            if (a < b && ((1 << i) & num1) == 0) {
+                res ^= 1 << i;
+                a++;
+            }
+        }
+        return res;
+    }
